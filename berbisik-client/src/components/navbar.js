d1fad52679df0ce1f3727e1641dbf1afc8306c97
ryanadhi <ryanadhi.p@gmail.com>
2020-05-08 15:35:25 +0700

    Release 5 * Initiate pages and components on React

---
+++ berbisik-client/src/components/navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "react-router-dom/Link";
+
+// Material UI
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+export default function Navbar() {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar className="nav-container">
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Signup
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
