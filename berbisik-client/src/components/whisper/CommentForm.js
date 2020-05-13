ab240d17acfec471f61ac2cd4a833537a4307ac3
ryanadhi <ryanadhi.p@gmail.com>
2020-05-14 00:29:25 +0700

    minor revision

--- berbisik-client/src/components/whisper/CommentForm.js
+++ berbisik-client/src/components/whisper/CommentForm.js
@@ -66,7 +66,7 @@ export default function CommentForm({ whisperId }) {
           <TextField
             name="body"
             type="text"
-            label="Comment on whisper"
+            label="Comment on Bisikan"
             error={errors.comment ? true : false}
             helperText={errors.comment}
             {...bindBody}
